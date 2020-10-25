aa8e6c07183c0aa6f1b9a03c82fd4e06633d71f1
Yaroslav <example@gmail.com>
2020-10-25 19:29:17 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//alert('Hello!');
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = prompt('Вам есть 18?', '');
+//console.log(typeof(answer));
+
+//const answers = [];
+
+//answers[0] = prompt('Ваше имя', '');
+//answers[1] = prompt('Ваша фамилия', '');
+//answers[2] = prompt('Ваш возраст', '');
+
+//document.write(answers);
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`);
+
+let  incr = 10;
+let decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
