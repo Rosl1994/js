ea9bdb2de77bfb116cad875e897d91b39bd907e5
Yaroslav <example@gmail.com>
2020-10-26 22:01:53 +0200

    practise one js

--- js/script.js
+++ js/script.js
@@ -20,11 +20,31 @@
 
 //console.log(`https://someurl.com/${category}/5`);
 
-let  incr = 10;
-let decr = 10;
+//let  incr = 10;
+//let decr = 10;
 
 //++incr;
 //--decr;
 
-console.log(++incr);
-console.log(--decr);
+//console.log(++incr);
+//console.log(--decr);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
